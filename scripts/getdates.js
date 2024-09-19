72eda811cb97f9e11f57eae88ab9eac3128c1eb6
nyasha <mrshepard18@gmail.com>
2024-09-19 12:44:50 +0200

    edit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically set the current year in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically set the date when the document was last modified
+document.getElementById("lastModified").textContent = "Last Updated: " + document.lastModified;
