541324ff7d9061358fa93b4e61f3272f1e60e898
nyasha <mrshepard18@gmail.com>
2024-09-27 19:07:59 +0200

    edit

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,10 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Dynamically set the date when the document was last modified
 document.getElementById("lastModified").textContent = "Last Updated: " + document.lastModified;
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.style.display = navMenu.style.display === 'block' ? 'none' : 'block';
+});
