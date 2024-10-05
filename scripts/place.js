35d9bb906a7056d83f85c45c77db001fb8059000
jambofy <mrshepard18@gmail.com>
2024-10-05 14:08:34 +0200

    edit

--- scripts/place.js
+++ scripts/place.js
@@ -1,7 +1,6 @@
 // Update footer year and last modified date
 document.getElementById('currentYear').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
-
 // Wind chill calculation
 function calculateWindChill(temperature, windSpeed) {
     if (temperature <= 10 && windSpeed > 4.8) {
@@ -9,11 +8,9 @@ function calculateWindChill(temperature, windSpeed) {
     }
     return "N/A";
 }
-
 // Set static weather values
 const temperature = 31; // °C
 const windSpeed = 5; // km/h
-
 // Calculate and display wind chill
 const windChill = calculateWindChill(temperature, windSpeed);
 document.getElementById('windChill').textContent = typeof windChill === 'number' ? windChill.toFixed(1) + "°C" : windChill;
